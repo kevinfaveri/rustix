0e3a74aa61d4cbaa8b53d267246cf290a865b502
Eric Santana <bitencourteric@hotmail.com>
2024-03-09 12:03:47 -0300

    fix: run server in 0.0.0.0 instead of 127.0.0.1

--- packages/backend/src/utils/server.rs
+++ packages/backend/src/utils/server.rs
@@ -4,8 +4,13 @@ use tracing::info;
 
 pub async fn serve_app(app: Router, port: u16) {
   info!("Server listening on 127, 0, 0, 1: {}", &port);
-
-  let addr = SocketAddr::from(([127, 0, 0, 1], port));
+  let local_address = SocketAddr::from(([127, 0, 0, 1], port));
+  let container_address = SocketAddr::from(([0, 0, 0, 0], port));
+  let addr = if cfg!(target_os = "linux") {
+    container_address
+  } else {
+    local_address
+  };
   let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
   axum::serve(listener, app)
     .await
